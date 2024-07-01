9c95b5712ac1f21e4b6cd445389620ba0db56e1a
KALIASALA <137120742+KALIASALA@users.noreply.github.com>
2024-07-01 02:29:54 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -44,40 +44,4 @@ function disableButtonForOneHour() {
 
 
 
-// Пример работы с Telegram Login Widget
-telegramLogin(authCallback);
 
-function authCallback(user) {
-    var telegramUserId = user.id; // Telegram user ID
-    // Проверяем, есть ли уже запись в базе данных для этого пользователя
-    // Если нет, создаем новую запись с начальным балансом
-    // Если есть, загружаем данные и отображаем на странице
-    loadUserData(telegramUserId);
-}
-
-function loadUserData(telegramUserId) {
-    // Здесь делаем запрос к серверу для загрузки данных пользователя по его Telegram user ID
-    // Например, через AJAX запрос к вашему API
-    fetch('/api/userdata?telegramUserId=' + telegramUserId)
-        .then(response => response.json())
-        .then(data => {
-            // Обновляем данные на странице
-            var coinCountSpan = document.getElementById('coin-count');
-            coinCountSpan.textContent = data.coinBalance; // Предполагается, что сервер вернет объект с балансом
-        })
-        .catch(error => console.error('Ошибка при загрузке данных пользователя', error));
-}
-
-function getCoin() {
-    // При нажатии на кнопку "GET SpinCoin"
-    // Отправляем запрос на сервер для увеличения баланса пользователя
-    var telegramUserId = getUserTelegramId(); // Функция, которая получает Telegram user ID пользователя
-    fetch('/api/getcoin?telegramUserId=' + telegramUserId)
-        .then(response => response.json())
-        .then(data => {
-            // Обновляем отображение баланса на странице
-            var coinCountSpan = document.getElementById('coin-count');
-            coinCountSpan.textContent = data.coinBalance;
-        })
-        .catch(error => console.error('Ошибка при получении SpinCoin', error));
-}
